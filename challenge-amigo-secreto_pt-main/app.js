3175c799b7d3a3e3ae2986cd001724dc8b23ee11
Rafael de Moraes <moraesbatistarafael@gmail.com>
2025-02-09 20:12:28 -0300

    codigo feito

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,50 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []; 
+
+
+function adicionarAmigo(){
+    let nome = document.querySelector ('input').value;
+    if (nome == ''){
+        alert ('coloque um nome valido por favor');
+    }else{
+    amigos.push(nome);
+    console.log (amigos);
+    listarNomes();
+    document.querySelector ('input').value ='';
+}
+}
+
+
+
+
+
+
+function sortearAmigo(){
+    if (amigos.length == 0) {
+        alert(' sem amigo não da pra fazer amigo secreto né');
+    }else {
+        escolherNome();
+}
+}  
+
+
+
+
+
+
+function listarNomes(){
+    let mostrarNomes = document.getElementById('listaAmigos');
+    mostrarNomes.innerHTML = (amigos);
+}
+
+
+
+function escolherNome(){
+  numero = parseInt(Math.random() * amigos.length);
+   console.log (numero);
+   let escolhido = amigos[numero];
+   alert (escolhido);
+    amigos.splice(numero, 1);
+    console.log (amigos);
+    listarNomes();
+}
